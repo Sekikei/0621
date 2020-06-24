7e298da3a43e6975bd79501545c77faa48b3334e
関口慶一 <sekiguchi@bpe.agr.hokudai.ac.jp>
2020-06-24 19:27:23 +0900

    プロジェクト ファイルを追加します。

---
+++ 0621/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _0621
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           char a= char.Parse(Console.ReadLine());
+            if (char.IsLower(a))
+            {
+                Console.WriteLine("a");
+            }
+            else
+            {
+                Console.WriteLine("A");
+            }
+        }
+    }
+}
